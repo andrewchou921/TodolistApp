ec151c48a548289a113847914f661067eb771bd8
Andrew <aa4269205@gmail.com>
2024-09-07 22:20:09 +0800

    路由改createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginFormView from '../views/loginformView.vue' // 將主頁改為登入頁面
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
