44e14530a661a4934d68533c58e56ee79059a3d1
Andrew <aa4269205@gmail.com>
2024-09-09 16:56:33 +0800

    把主視覺圖片置換

--- src/views/loginformView.vue
+++ src/views/loginformView.vue
@@ -6,7 +6,8 @@
         <a href="#">
           <img class="logoImg" src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png" alt=""/>
         </a>
-        <img class="d-m-n" src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png" alt="workImg" />
+        <!-- <img class="d-m-n" src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png" alt="workImg" /> -->
+        <img class="d-m-n" src="/bg.png" alt="workImg" />
       </div>
       <div>
         <form class="formControls" @submit.prevent="signIn">
